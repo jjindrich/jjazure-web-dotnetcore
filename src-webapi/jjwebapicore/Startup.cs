4c739cb0a9f76abd1cd80aa8108987d55522700b
Jaroslav Jindrich <jajindri@microsoft.com>
2020-01-27 18:10:55 +0100

    jjwebapicore - dotNet Core 3

--- src-webapi/jjwebapicore/Startup.cs
+++ src-webapi/jjwebapicore/Startup.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using jjwebapicore.Models;
 
 namespace jjwebapicore
@@ -27,7 +27,7 @@ namespace jjwebapicore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers();
 
             // load connection string from ENV or from appsettings.json
             string connStr = Environment.GetEnvironmentVariable("ConnectionStrings_ContactsContext");
@@ -40,19 +40,23 @@ namespace jjwebapicore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
