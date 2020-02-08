cf574cebc4576ba9eeb3816ae54290c0f18c08e2
Jaroslav Jindrich <jajindri@microsoft.com>
2020-02-08 17:06:58 +0100

    contact list

---
+++ src-webui/jjwebcore/Controllers/ContactsController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace jjwebcore.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private jjwebapicore.ContactsClient cl = new jjwebapicore.ContactsClient();
+
+        public async Task<IActionResult> Index()
+        {
+            var contacts = await cl.GetContactAllAsync();
+            return View(contacts);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var c = await cl.GetContactAsync(id);
+            return View(c.);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
