4ee80f7284c9f52cf5a37fc5781d65d0e9864a86
Jaroslav Jindrich <jajindri@microsoft.com>
2020-03-23 19:10:15 +0100

    jjwebapicore sql no identity

--- src-webapi/jjwebapicore/Models/Contacts.cs
+++ src-webapi/jjwebapicore/Models/Contacts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +9,8 @@ namespace jjwebapicore.Models
 {
     public class Contact
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ContactId { get; set; }
         public string FullName { get; set; }
-    }
+     }
 }
