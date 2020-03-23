4528bfddac555eb59cb838822117b9e3f708dc07
Jaroslav Jindrich <jajindri@microsoft.com>
2020-03-23 13:02:22 +0100

    jjwebapicore change values controller

--- src-webapi/jjwebapicore/Controllers/ValuesController.cs
+++ src-webapi/jjwebapicore/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace jjwebapicore.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value10", "value20" };
+            return new string[] { "value11", "value22" };
         }
 
         // GET api/values/5
