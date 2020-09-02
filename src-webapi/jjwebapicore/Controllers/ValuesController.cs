b6629be62d1db73dc7627e0ee2e14af914c80531
Jaroslav Jindrich <jajindri@microsoft.com>
2020-09-02 14:26:51 +0200

    webapi - change values

--- src-webapi/jjwebapicore/Controllers/ValuesController.cs
+++ src-webapi/jjwebapicore/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace jjwebapicore.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value20200902" };
+            return new string[] { "value1", "value21" };
         }
 
         // GET api/values/5
