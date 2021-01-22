57489eba82ef8e2fc4f0aae3069cc0ee1e4ed396
Jaroslav Jindrich <jajindri@microsoft.com>
2021-01-22 14:32:00 +0100

    jmeter tf

--- jmeter-loadtest/terraform/provider.tf
+++ jmeter-loadtest/terraform/provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "jjdevmanagement"
+    storage_account_name = "jjtfstate"
+    container_name       = "jmeter"
+    key                  = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm    = "~> 2.26"
+  }
+}
+
 provider "azurerm" {
   version = "=2.26.0"
   features {}
