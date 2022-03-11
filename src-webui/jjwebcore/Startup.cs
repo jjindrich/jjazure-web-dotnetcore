7a0ba62ddc52d921636ff6759de988e54349180e
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-03-11 22:39:57 +0100

    Use Forwarded hearders

--- src-webui/jjwebcore/Startup.cs
+++ src-webui/jjwebcore/Startup.cs
@@ -80,10 +80,12 @@ namespace jjwebcore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseForwardedHeaders();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseForwardedHeaders();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
