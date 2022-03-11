dd8a913fb663ceb3ad7bb0bb38115f1576523eb2
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-03-11 22:48:43 +0100

    Use forwareder

--- src-webui/jjwebcore/Startup.cs
+++ src-webui/jjwebcore/Startup.cs
@@ -53,6 +53,13 @@ namespace jjwebcore
             });
 
             // use Azure B2C
+            services.Configure<ForwardedHeadersOptions>(options =>
+ {
+   options.ForwardedHeaders =
+       ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+   options.KnownNetworks.Clear();
+   options.KnownProxies.Clear();
+ });
             // docs https://docs.microsoft.com/en-us/azure/active-directory-b2c/enable-authentication-web-application?tabs=visual-studio
             services.Configure<CookiePolicyOptions>(options =>
             {
