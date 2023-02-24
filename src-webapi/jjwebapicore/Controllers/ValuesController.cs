ed416ad27bcc453d5a91d24121fad3abd7611f50
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2023-02-24 14:42:11 +0100

    fix pipeline dependency

--- src-webapi/jjwebapicore/Controllers/ValuesController.cs
+++ src-webapi/jjwebapicore/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace jjwebapicore.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "dotnet7" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
