92f08c60faa65c0382cbbe2275cf82d93b2ea16e
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2024-06-10 10:53:05 +0200

    playwright test code

--- playwright-test/playwright.config.ts
+++ playwright-test/playwright.config.ts
@@ -1,39 +1,37 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
-    testDir: './tests',
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
-    use: {
-        trace: 'on-first-retry',
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+        name: 'Microsoft Edge',
+        use: {
+            // Supported Microsoft Edge channels are: msedge, msedge-beta, msedge-dev, msedge-canary
+            channel: 'msedge',
+        },
     },
-    projects: [
-        {
-            name: 'Microsoft Edge',
-            use: {
-                // Supported Microsoft Edge channels are: msedge, msedge-beta, msedge-dev, msedge-canary
-                channel: 'msedge',
-            },
-        },
+    
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
-
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
-    ],
-};
-
-export default config
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+  ],
+});
--- playwright-test/tests/jjweb.spec.ts
+++ playwright-test/tests/jjweb.spec.ts
@@ -1,14 +1,22 @@
-// tests/foo.spec.ts
 import { test, expect } from '@playwright/test';
 
 test('jjweb test', async ({ page }) => {
   var urlRoot = 'https://jjazgwaks.germanywestcentral.cloudapp.azure.com';
 
   await page.goto(urlRoot);
+
+  // if you comment, the test will fail
+  const consent = page.locator("#cookieConsent")
+  if (consent){
+    await expect(consent).toContainText('privacy');
+    await page.getByRole('button', { name: 'Accept' }).click();  
+  }
+
   const title = page.locator('footer');
   await expect(title).toContainText('JJ');
 
-  await page.goto(urlRoot + '/home/test/');
+  await page.getByRole('link', { name: 'Test' }).click();
+
   const api = page.locator('.body-content');
   await expect(api).toContainText('value1');
 
